8632ace3c9f80257d5f3f6773c241069a3b09d25
Darobinson20 <Darobinson20@gmail.com>
2022-04-29 02:04:58 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,98 +1,100 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generatePage = require('./page-template');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [{
+  type: 'input',
+  name: 'Title',
+  message: "What is the name of your project? (Required)",
+  validate: nameInput => {
+    if (nameInput){
+      return true;
+    } else {
+      console.log('Please enter name of project!');
+      return false;
+    }
+  }
+},
+{
+  type: 'input',
+  name: 'Description',
+  message: 'What was your motivation?'
+},
+{
+  type: 'input',
+  name: 'name',
+  message: 'Why did you build this project?'
+},
+{
+  type: 'input',
+  name: 'name',
+  message: 'What problem does it solve?'
+},
+{
+  type: 'input',
+  name: 'name',
+  message: 'What did you learn?'
+},
+{
+  type: 'input',
+  name: 'Installation',
+  message: 'What are the steps to install your project?'
+},
+{
+  type: 'input',
+  name: 'Usage',
+  message: 'Provide instructions and examples for use.'
+},
+{
+  type: 'input',
+  name: 'License',
+  message: ''
+},
+{
+  type: 'input',
+  name: 'Contributing',
+  message: 'How can you contribute to this project? '
+},
+{
+  type: 'confirm',
+  name: 'Test',
+  message: 'Have you tested the project prior to deploying?',
+  default: false
+},
+{
+  type: 'input',
+  name: 'Questions',
+  message: 'Enter your GitHub Username'
+},
+{
+  type: 'link',
+  name: 'Questions',
+  message: 'If you have any additional questions, you can reach me @'
+}
+
+];
 
 
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'Title',
-      message: 'What is the name of your project? (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter name of project!');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'Description',
-      message: 'What was your motivation?'
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Why did you build this project?'
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What problem does it solve?'
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What did you learn?'
-    },
-    {
-      type: 'input',
-      name: 'Installation',
-      message: 'What are the steps to install your project?'
-    },
-    {
-      type: 'input',
-      name: 'Usage',
-      message: 'Provide instructions and examples for use.'
-    },
-    {
-      type: 'input',
-      name: 'License',
-      message: ''
-    },
-    {
-      type: 'input',
-      name: 'Contributing',
-      message: 'How can you contribute to this project? '
-    },
-    {
-      type: 'confirm',
-      name: 'Test',
-      message: 'Have you tested the project prior to deploying?',
-      default: false
-    },
-    {
-      type: 'input',
-      name: 'Questions',
-      message: 'Enter your GitHub Username'
-    },
-    {
-      type: 'link',
-      name: 'Questions',
-      message: 'If you have any additional questions, you can reach me @'
-    },
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
 
+  fs.writeFile(fileName, data, err => {
+    if (err) throw err;
 
-  ]);
-};
-
-promptUser()
-.then(portfololioData => {
-  const pageHTML = generatePage(portfololioData);
-
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('README complete! Check out index.html to see your work!');
-});
+    console.log('README complete!');
+   });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+  .then(function(data) {
+    writeToFile('README.md', generateMarkdown(data));
+    console.log(data);
+  });
+}
 
 // Function call to initialize app
 init();
-})
